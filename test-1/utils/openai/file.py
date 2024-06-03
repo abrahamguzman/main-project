1adbbab079c09cf5639e118351d082ede461135b
mguzmanicg <mguzman@icgsoftwareperu.com>
2024-06-03 16:53:49 -0500

    feat: Crea interface para asistente

--- test-1/utils/openai/file.py
+++ test-1/utils/openai/file.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+from utils.logger import logger
 
 client = OpenAI()
 
@@ -9,6 +10,23 @@ def subir_archivo(client, ruta_archivo):
         purpose="assistants"
         )
     return response
+
+
+def subir_archivos(client, rutas_archivos: list):
+    id_archivos = []
+    for ruta_archivo in rutas_archivos:
+        try:
+            response = client.files.create(
+                file=open(ruta_archivo, "rb"), 
+                purpose="assistants"
+                )
+            id_archivos.append(response.id)
+            logger.info(f"Archivo subido: {response.id}")
+        except Exception as e:
+            logger.error(f"Error al subir archivo: {e}")
+            continue
+    return id_archivos
+
 
 def listar_archivos(client):
 	response = client.files.list()
