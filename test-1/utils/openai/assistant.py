0b0f340ac32cec293f36b04381e2a465ff0ff523
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2024-06-06 22:28:15 -0500

    fix: Mejora asistente

--- test-1/utils/openai/assistant.py
+++ test-1/utils/openai/assistant.py
@@ -1,12 +1,15 @@
+from config.openai import client_ai
+
+
 def crear_asistente(
-  client, 
-  nombre, 
-  instrucciones, 
-  temperatura=0,
-  model="gpt-4o", 
-  vector_store_id=None,
-  top_p=0
-  ):
+    client,
+    nombre,
+    instrucciones,
+    temperatura=0,
+    model="gpt-4o",
+    vector_store_id=None,
+    top_p=0,
+):
     response = client.beta.assistants.create(
         name=nombre,
         model=model,
@@ -17,7 +20,18 @@ def crear_asistente(
         top_p=top_p,
     )
     return response
-  
+
+
 def listar_asistentes(client):
     response = client.beta.assistants.list(order="desc")
     return response
+
+
+def modificar_asistente(id_asistente, instrucciones, model="gpt-4o"):
+    client_ai.beta.assistants.update(
+        id_asistente,
+        instructions=instrucciones,
+        model=model,
+    )
+    
+    print("Asistente modificado con éxito.")
